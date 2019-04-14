b7f47c666ee2f96c698fd52caabb21d68901466f
Cyril Marchive <cyril.marchive@gmail.com>
2019-04-14 18:09:45 -0400

    Creation des actions

---
+++ src/actions/index.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import {AT_POSTS} from "./action-types";
+
+const END_POINT = "http://localhost:8080";
+
+export function readAllPost() {
+    return function (dispatch) {
+        axios.get(`${END_POINT}/posts`).then(response => {
+            dispatch({
+                type: AT_POSTS.READ_ALL,
+                payload: response
+            })
+        });
+    }
+}
+
+export function readPost(id) {
+    return function (dispatch) {
+        axios.get(`${END_POINT}/posts/${id}`).then(response => {
+            dispatch({
+                type: AT_POSTS.READ,
+                payload: response
+            })
+        });
+    }
+}
+
+export function deletePost(id) {
+    return function (dispatch) {
+        axios.delete(`${END_POINT}/posts/${id}`).then(response => {
+            dispatch({
+                type: AT_POSTS.READ_ALL,
+                payload: id
+            })
+        });
+    }
+}
+
+export function createPost(post) {
+    return function (dispatch) {
+        axios.post(`${END_POINT}/posts/`,
+            {
+                title: post.title,
+                content: post.content,
+                author: post.author
+            }
+        ).then(response => {
+            dispatch({
+                type: AT_POSTS.CREATE,
+                payload: response.data
+            })
+        });
+    }
+}
